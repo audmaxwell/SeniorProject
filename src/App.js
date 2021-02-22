0ffc3e32fafb1968819d4b985c9c9c7d54e63b06
audmaxwell <snowberry642@gmail.com>
2021-02-22 15:12:21 -0500

    0m 0m mgh ef hj,gtrdnhjm Y'nmhg :wq

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="initialScreen">
+      <h1>Welcome!</h1>
+      <form>
+        <h2 className="label-wrapper">
+          <label htmlFor="loginInput" className="label__userlog">
+            who are u
+          </label>
+        </h2>
+        <input
+          placeholder="username"
+          type="text"
+          id="loginInput"
+          className="input input__lg"
+          name="text"
+          autoComplete="off"
+        />
+        <div>
+        <input
+          placeholder="password"
+          type="text"
+          id="loginInput"
+          className="input input__lg"
+          name="text"
+          autoComplete="off"
+        />
+        </div>
+        <div>
+        <button type="submit" className="btn btn__primary btn__lg">
+          Login
+        </button>
+        </div>
+      </form>
     </div>
   );
 }
