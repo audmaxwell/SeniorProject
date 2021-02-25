5d35781c8001fdf52b72d4f297a8232aa1f71330
Audrey Maxwell <amaxwell@osprey.goucher.edu>
2021-02-25 13:28:37 -0500

    connected to db

--- server.js
+++ server.js
@@ -2,10 +2,33 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const path = require('path');
 const app = express();
+
+var mysql = require('mysql');
+var conn = mysql.createConnection({
+  host: 'localhost', 
+  user: 'root',
+  password: 'SelectPink',
+  database: 'firstDB'
+}); 
+conn.connect(function(err) {
+  if (err) throw err;
+  console.log('Database is connected successfully !');
+});
 app.use(express.static(path.join(__dirname, 'build')));
+app.post('/create-user', (req, res) => {
+  const userDetails=req.body;
+  var sql = 'INSERT INTO user SET ?';
+  console.log(userDetails)
+  conn.query(sql, userDetails,function (err, data) { 
+      if (err) throw err;
+         console.log("User data is inserted successfully "); 
+  });
+// res.redirect('/users/form')
+
+})
 
 app.get('/ping', function (req, res) {
- return res.send('pong');
+ return res.json('pong');
 });
 
 app.get('/', function (req, res) {
--- src/App.js
+++ src/App.js
@@ -1,36 +1,53 @@
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(function(){
+    fetch("/ping").then((res)=> res.json()).then(console.log)
+  });
   return (
     <div className="initialScreen">
       <h1>Welcome!</h1>
-      <form>
+      <form action="/create-user" method="POST">
         <h2 className="label-wrapper">
           <label htmlFor="loginInput" className="label__userlog">
-            who are u
+            Sign up
           </label>
         </h2>
         <input
-          placeholder="username"
+          placeholder="email"
           type="text"
-          id="loginInput"
+          id="emailReg"
           className="input input__lg"
           name="text"
           autoComplete="off"
+          required
         />
+        <div>
+        <input
+          placeholder="username"
+          type="text"
+          id="usernameReg"
+          className="input input__lg"
+          name="text"
+          autoComplete="off"
+          required
+        />
+        </div>
         <div>
         <input
           placeholder="password"
           type="text"
-          id="loginInput"
+          id="passwordReg"
           className="input input__lg"
           name="text"
           autoComplete="off"
+          required
         />
         </div>
         <div>
         <button type="submit" className="btn btn__primary btn__lg">
-          Login
+          Submit
         </button>
         </div>
       </form>
