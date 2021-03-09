adc633b3e44b97efe925fc78ab9e430fd32e48cf
Audrey Maxwell <amaxwell@osprey.goucher.edu>
2021-03-09 13:21:19 -0500

    hi

--- server.js
+++ server.js
@@ -2,27 +2,37 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const path = require('path');
 const app = express();
-
 var mysql = require('mysql');
 var conn = mysql.createConnection({
   host: 'localhost', 
   user: 'root',
   password: 'SelectPink',
-  database: 'firstDB'
+  database: 'newDB'
 }); 
 conn.connect(function(err) {
   if (err) throw err;
   console.log('Database is connected successfully !');
 });
+module.exports = conn;
+app.use(express.urlencoded({
+  extended: true
+}));
 app.use(express.static(path.join(__dirname, 'build')));
-app.post('/create-user', (req, res) => {
-  const userDetails=req.body;
-  var sql = 'INSERT INTO user SET ?';
-  console.log(userDetails)
-  conn.query(sql, userDetails,function (err, data) { 
-      if (err) throw err;
+app.post('/registration/create', (req, res) => {
+  console.log(req.body);
+  const { email, username, password } = req.body;
+  var sql = `INSERT INTO users (
+    email,username, password)
+    VALUES(
+    ?, ?, ?)`;
+  conn.query(sql, [email, username, password], function (err, data) {
+    if (err) throw err;
          console.log("User data is inserted successfully "); 
   });
+//  conn.query(sql, userDetails,function (err, data) { 
+ //     if (err) throw err;
+ //        console.log("User data is inserted successfully "); 
+//  });
 // res.redirect('/users/form')
 
 })
--- src/App.js
+++ src/App.js
@@ -1,56 +1,30 @@
 import './App.css';
-import { useEffect } from 'react';
-
+import { useEffect, useState } from 'react';
+import Header from './components/header';
+import Register from './routes/registration';
+import Homepage from './routes/homepage';
+import {BrowserRouter as Router,Switch, Route} from 'react-router-dom';
 function App() {
   useEffect(function(){
     fetch("/ping").then((res)=> res.json()).then(console.log)
   });
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const page = isLoggedIn ?<Homepage/> : <Register onLogin ={()=>setIsLoggedIn(true)}/>;
+  console.log(isLoggedIn);
   return (
     <div className="initialScreen">
+      <div className="Wrapper">
+        <Header/>
+      </div>
       <h1>Welcome!</h1>
-      <form action="/create-user" method="POST">
-        <h2 className="label-wrapper">
-          <label htmlFor="loginInput" className="label__userlog">
-            Sign up
-          </label>
-        </h2>
-        <input
-          placeholder="email"
-          type="text"
-          id="emailReg"
-          className="input input__lg"
-          name="text"
-          autoComplete="off"
-          required
-        />
-        <div>
-        <input
-          placeholder="username"
-          type="text"
-          id="usernameReg"
-          className="input input__lg"
-          name="text"
-          autoComplete="off"
-          required
-        />
-        </div>
-        <div>
-        <input
-          placeholder="password"
-          type="text"
-          id="passwordReg"
-          className="input input__lg"
-          name="text"
-          autoComplete="off"
-          required
-        />
-        </div>
-        <div>
-        <button type="submit" className="btn btn__primary btn__lg">
-          Submit
-        </button>
-        </div>
-      </form>
+      {page}
+      {/* <Router>
+          <Switch>3
+            <Route path='/homepage'><Homepage /></Route>
+            <Route exact path='/'><Register /></Route>
+          </Switch>
+      </Router>
+      */}
     </div>
   );
 }
